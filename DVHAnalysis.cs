e00ab836a8a7733fb14aeb32d15b65e2788268c6
Ben Catt <bcatt09@gmail.com>
2018-06-19 13:53:29 -0400

    added printing and removed DVH window

--- DVHAnalysis.cs
+++ DVHAnalysis.cs
@@ -70,14 +70,16 @@ namespace VMS.TPS
 				MessageBox.Show("Please open a plan or plan sum before running this script", "No Active Plan", MessageBoxButton.OK, MessageBoxImage.Error);
 				throw new ApplicationException("Please open a plan or a plan sum before running this script");
 			}
-			window.Title = "DVH Analysis Window";
+			window.Title = "DVH Analysis - " + pat.LastName + ", " + pat.FirstName + " (" + pat.Id + ")";
+			window.Width = 750;
+			window.Height = 955;
 
 			MainWindow userControl = new MainWindow();
 			DVHViewModel viewModel = new DVHViewModel(pat, pItem, course, ss);
 
 			window.Content = userControl;
 			window.DataContext = viewModel;
-			userControl.PlanInfoListBox.DataContext = viewModel.Plans;
+			//userControl.PlanInfoListBox.DataContext = viewModel.Plans;
 			userControl.DVHDataGrid.DataContext = viewModel.DVHTable;
 
 			/*DVHData dvhData = pItem.GetDVHCumulativeData((from s in (pItem as PlanSetup).StructureSet.Structures where s.Id == (pItem as PlanSetup).TargetVolumeID select s).First(),
